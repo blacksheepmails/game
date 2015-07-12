b76c86ff0f5616fc8a649713e4c9b0c320f47380
sheep <blacksheepmails@gmail.com>
2015-07-12 16:54:25 +0100

    woops forgot to add the other files

--- js/drawing.js
+++ js/drawing.js
@@ -1,34 +1,38 @@
 var drawing = function(ctx){
-	var drawSquare = function(x, y, color) {
+	var drawSquare = function(i, j, color) {
 	    ctx.fillStyle = color;
-	    ctx.fillRect(x * square_size, y * square_size, square_size, square_size);
+	    ctx.fillRect(i * square_size, j * square_size, square_size, square_size);
 	};
 
 	var drawBoard = function() {
 		var color = 'red';
-		for (var x = 1; x <= 8; x += 1) {
+		for (var i = 1; i <= 8; i += 1) {
 			color = toggleColor(color);
-		    for (var y = 1; y <= 8; y += 1) {
+		    for (var j = 1; j <= 8; j += 1) {
 		        color = toggleColor(color);
-		        drawSquare(x, y, color);
+		        drawSquare(i, j, color);
 		    }
 		}
-
-		for (var y = 1; y <= 8; y++) {
-		 	ctx.fillText(y.toString(), 10, (y+1) * square_size);
-		 	ctx.fillText(y.toString(), board_size-20, (y+1) * square_size);
+		for (var i = 1; i <= 8; i++) {
+		 	var letter = String.fromCharCode(96+i);
+		  	ctx.fillText(letter, i * square_size, 20);
+		 	ctx.fillText(letter, i * square_size, board_size-10);
 		}
-
-		for (var x = 1; x <= 8; x++) {
-		 	var letter = String.fromCharCode(96+x);
-		  	ctx.fillText(letter, x * square_size, 20);
-		 	ctx.fillText(letter, x * square_size, board_size-10);
+		for (var j = 1; j <= 8; j++) {
+		 	ctx.fillText(j.toString(), 10, (j+1) * square_size);
+		 	ctx.fillText(j.toString(), board_size-20, (j+1) * square_size);
 		}
 	};
 
+	var drawPieces = function(pieces) {
+		for (var i = 0; i < pieces.length; i++) {
+			pieces[i].draw();
+		}
+	}
 
 	return {
 		drawSquare: drawSquare,
-		drawBoard: drawBoard
+		drawBoard: drawBoard,
+		drawPieces: drawPieces
 	};
 };
--- js/main.js
+++ js/main.js
@@ -1,7 +1,73 @@
 
 var square_size = 240/8.0;
 var board_size = 300;
+var game_pieces = [];
 
+var offset = function (canvas) {
+	var x = 0;
+	var y = 0;
+
+	stylePaddingLeft = parseInt(document.defaultView.getComputedStyle(canvas, null)['paddingLeft'], 10)      || 0;
+    stylePaddingTop  = parseInt(document.defaultView.getComputedStyle(canvas, null)['paddingTop'], 10)       || 0;
+    styleBorderLeft  = parseInt(document.defaultView.getComputedStyle(canvas, null)['borderLeftWidth'], 10)  || 0;
+    styleBorderTop   = parseInt(document.defaultView.getComputedStyle(canvas, null)['borderTopWidth'], 10)   || 0;
+
+    var element = canvas;
+	if (element.offsetParent) {
+        do {
+            x += element.offsetLeft;
+            y += element.offsetTop;
+        } while ((element = element.offsetParent));
+    }
+
+	x += stylePaddingLeft;
+    y += stylePaddingTop;
+    x += styleBorderLeft;
+    y += styleBorderTop;
+    return {
+    	x: x,
+    	y: y
+    };
+}
+
+function getSquare(x, y) {
+	var i = Math.floor(x/square_size);
+	var j = Math.floor(y/square_size);
+	if (i >= 1 && i <= 8 && j >= 1 && j <= 8) {
+		return {
+			i: i,
+			j: j
+		};
+	} else {
+		return 'None';
+	}
+}
+
+function getGamePiece(i, j) {
+	for (var c = 0; c < game_pieces.length; c++) {
+		if (game_pieces[c].i == i && game_pieces[c].j == j) {
+			return game_pieces[c]
+		}
+	}
+	return 'None'
+}
+
+function mouseDown(e) {
+	var canvas = document.getElementById('myCanvas');
+	var square = getSquare(e.pageX - offset(canvas).x, e.pageY - offset(canvas).y);
+	if (square != 'None') {
+		var game_piece = getGamePiece(square.i, square.j);
+		if (game_piece != 'None') {
+			console.log('clicked on a game piece!');
+			game_piece.isActive = true;
+			game_piece.draw();
+		} else {
+			console.log('clicked on a square thats not a game piece!');
+		}
+	} else {
+		console.log('watch where you click, bitch');
+	}
+}
 
 var toggleColor = function(color) {
     if (color === 'red') {
@@ -11,36 +77,49 @@ var toggleColor = function(color) {
     }
 };
 
-var GamePiece = function(ctx, x, y, piece, color) {
+var GamePiece = function(ctx, i, j, piece, color) {
 	this.ctx = ctx
-	this.x = x;
-	this.y = y;
+	this.i = i;
+	this.j = j;
 	this.piece = piece;
 	this.color = color;
+	this.isActive = false;
 
 	this.draw = function() {
-		ctx.drawImage(this.piece, this.x * square_size, this.y * square_size, square_size, square_size);
+		if (this.isActive) {
+			ctx.lineWidth="5";
+			ctx.strokeStyle="yellow";
+			ctx.rect(this.i * square_size, this.j * square_size, square_size, square_size);
+			ctx.stroke();
+			ctx.drawImage(this.piece, this.i * square_size, this.j * square_size, square_size, square_size);
+		} else {
+			ctx.drawImage(this.piece, this.i * square_size, this.j * square_size, square_size, square_size);
+		}
 	};
 };
 
 
 var main = function(){
-	var c = document.getElementById("myCanvas");
-
-	var ctx = c.getContext("2d");
+	var canvas = document.getElementById("myCanvas");
+	var ctx = canvas.getContext("2d");
 	ctx.font = '20px Arial';
 
-	
+	var blackCheckerImg = new Image();
+	blackCheckerImg.src = "black_checker.png";
+	var redCheckerImg = new Image();
+	redCheckerImg.src = 'red_checker.png';
+
+	game_pieces.push (new GamePiece(ctx, 2, 2, blackCheckerImg, 'black'));
+	game_pieces.push (new GamePiece(ctx, 2, 4, blackCheckerImg, 'black'));
+	game_pieces.push (new GamePiece(ctx, 2, 5, redCheckerImg, 'red'));
+	game_pieces.push (new GamePiece(ctx, 2, 6, blackCheckerImg, 'black'));
+	game_pieces.push (new GamePiece(ctx, 2, 8, blackCheckerImg, 'black'));
+	game_pieces.push (new GamePiece(ctx, 2, 10, blackCheckerImg, 'black'));
+
 	drawing(ctx).drawBoard();
-
-	var blackChecker = new Image();
-	blackChecker.src = "black_checker.png";
-
-	var blackPiece = new GamePiece(ctx, 2, 2, blackChecker, 'black');
-	blackChecker.onload = function(){
-		blackPiece.draw();
-	};
-
+	canvas.onmousedown = mouseDown;
+	redCheckerImg.onload = drawing(ctx).drawPieces.bind(this, game_pieces);
+	//setInterval(drawing(ctx).drawPieces.bind(this, game_pieces), 1000);
 };
 
 main();
