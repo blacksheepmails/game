6f756fd43807b47c405543b20fb091aef8ca52fd
eeue56 <eeue56@users.noreply.github.com>
2015-07-17 22:30:59 +0100

    refactoring

---
+++ js/pieces.js
@@ -0,0 +1,134 @@
+var Pieces = function(game_pieces){
+
+    CheckersKingPiece = function(ctx, i, j, piece, color) {
+        GamePiece.call(this, ctx, i, j, piece, color);
+        this.possibleMoves = function() {
+            var getPiece = getGamePiece.bind(this, game_pieces);
+            var moves = [];
+
+            for (var i_dir = -1; i_dir <= 1; i_dir += 2) {
+                for (var j_dir = -1; j_dir <= 1; j_dir += 2) {
+
+                    var piece = getPiece(this.i + i_dir, this.j + j_dir);
+                    
+                    if (piece == null) {
+                        moves.push({
+                            i: this.i + i_dir, 
+                            j: this.j + j_dir, 
+                            sideEffects: []
+                        });
+                    } else if (piece.color != color) {
+                        if (getPiece(this.i + 2 * i_dir, this.j + 2 * j_dir) == null) {
+                            moves.push({
+                                i: this.i + 2 * i_dir, 
+                                j: this.j + 2 * j_dir, 
+                                sideEffects: [capturePiece.bind(this, game_pieces, piece)]
+                            });
+                        }
+                    }
+                }
+            }
+            return moves;
+        }
+    }
+
+    CheckersPiece = function(ctx, i, j, piece, color) {
+        GamePiece.call(this, ctx, i, j, piece, color);
+
+        this.possibleMoves = function() {
+            var getPiece = getGamePiece.bind(this, game_pieces);
+
+            var moves = [];
+            var j_dir = (color == 'black')? -1: 1;
+
+            for (var i_dir = -1; i_dir <= 1; i_dir += 2) {
+                var piece = getPiece(this.i + i_dir, this.j + 1 * j_dir);
+
+                if (piece == null) {
+                    moves.push({
+                        i: this.i + i_dir, 
+                        j: this.j + 1 * j_dir, 
+                        sideEffects: []
+                    });
+                } else if (piece.color != color) {
+                    if (getPiece(this.i + 2 * i_dir, this.j + 2 * j_dir) == null) {
+                        moves.push({
+                            i: this.i + 2 * i_dir, 
+                            j: this.j + 2 * j_dir, 
+                            sideEffects: [capturePiece.bind(this, game_pieces, piece)]
+                        });
+                    }
+                }
+            }
+
+
+            for (var c = 0; c < moves.length; c++) {
+                if (this.isLastRow(moves[c].j)) {
+                    moves[c].sideEffects.push(this.kingMe.bind(this, moves[c]));
+                }
+            }
+
+            return moves;
+        };
+
+        this.isLastRow = function(j) {
+            if (color == 'black' && j == 1 || color == 'red' && j == 8) {
+                return true;
+            }
+            return false;
+        };
+
+        this.kingMe = function(move) {
+            var checkerImg = (this.color == 'black') ? blackCheckerKing : redCheckerKing;
+            game_pieces.splice(game_pieces.indexOf(this), 1, new CheckersKingPiece(this.ctx, move.i, move.j, checkerImg, this.color));
+        };
+    }
+
+    GamePiece.prototype = {
+        isActive: false,
+        possibleMoves: function() {
+            return null;
+        },
+        isValidMove: function(i, j) {
+            var validMoves = this.possibleMoves();
+
+            for (var c = 0; c < validMoves.length; c++) {
+                if (validMoves[c].i == i && validMoves[c].j == j) {
+                    console.log(i.toString() + ', ' + j.toString() + ' is a valid move');
+                    return true;
+                }
+            }
+            return false;
+        },
+        getMove: function(i, j) {
+            //should save possible moves instead of clculating them each time. anyways make this better later.
+            var validMoves = this.possibleMoves();
+
+            for (var c = 0; c < validMoves.length; c++) {
+                if (validMoves[c].i == i && validMoves[c].j == j) {
+                    return validMoves[c];
+                }
+            }
+        },
+        highlight: function(){
+            this.ctx.lineWidth = "5";
+            this.ctx.strokeStyle = "yellow";
+            this.ctx.strokeRect(this.i * square_size, this.j * square_size, square_size, square_size);
+        },
+        draw : function() {
+            if (this.isActive) {
+                this.highlight();
+            }
+
+            this.ctx.drawImage(this.piece, this.i * square_size, this.j * square_size, square_size, square_size);
+        }
+    }
+
+    CheckersKingPiece.prototype = Object.create(GamePiece.prototype);
+    CheckersPiece.prototype = Object.create(GamePiece.prototype);
+
+    return {
+        CheckersKingPiece: CheckersKingPiece,
+        CheckersPiece: CheckersPiece
+    };
+};
