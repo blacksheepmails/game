d48c67c4774033dd552f2d95139c35e61fa239a2
Maslab eeePC <blacksheepmails@gmail.com>
2015-07-23 17:09:14 +0100

    deleted some not needed code for isValidMove

--- static/js/chessStateMachines.js
+++ static/js/chessStateMachines.js
@@ -41,27 +41,6 @@ NormalChessStateMachine.prototype = Object.create(ChessStateMachine.prototype);
 NormalChessStateMachine.prototype.isValidMove = function(piece, i, j) {
     if (!piece.isValidMove(i, j)) return false;
     return piece.player.isValidMove(piece, i, j);
-    var old_i = piece.i;
-    var old_j = piece.j;
-    var effects = piece.getMove(i, j).sideEffects;
-    var temp = null;
-    for (var c = 0; c < effects.length; c++) {
-        if (effects[c] instanceof this.pieceNamespace.Capture) {
-            temp = effects[c].captured;
-            effects[c].go();
-        }
-    }
-    piece.i = i;
-    piece.j = j;
-
-    this.updatePossibleMoves();
-    var returnVal = !this.whoseTurn.isInCheck()
-    piece.i = old_i;
-    piece.j = old_j;
-    if (temp != null) this.game_pieces.push(temp);
-    this.updatePossibleMoves();
-
-    return returnVal;
 };
 NormalChessStateMachine.prototype.updatePossibleMoves = function() {
     ChessStateMachine.prototype.updatePossibleMoves.call(this);
